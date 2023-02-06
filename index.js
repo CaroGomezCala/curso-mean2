0d2edaeaf60ddd854f135f04aa6e5d2018ce04a9
CaroGomezCala <lcgk.021@gmail.com>
2023-02-05 21:04:59 -0500

    crear servidor web

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
-var mongoose = requiere('mongoose');
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3977;
 
 //conexion base de datos
 mongoose.connect('mongobd://localhost:27017/curso_mean2', (err,res)=>{
@@ -9,5 +11,9 @@ mongoose.connect('mongobd://localhost:27017/curso_mean2', (err,res)=>{
     }
     else{
         console.log("La conexion a la base de datos esta corriendo correctamente...");
+        app.listen(port, function()
+        {
+            console.log("Servidor del API Rest escuchando en http://localhost:"+port);
+        })
     }
 });
